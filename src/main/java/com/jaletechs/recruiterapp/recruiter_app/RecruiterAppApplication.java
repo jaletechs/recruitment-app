e98b8895ab9b82a5921b544c3a22594f0ce435a7
Adamson Jeremiah <jaletechs@gmail.com>
2019-06-15 16:08:06 +0100

    initial commit

---
+++ src/main/java/com/jaletechs/recruiterapp/recruiter_app/RecruiterAppApplication.java
@@ -0,0 +1,13 @@
+package com.jaletechs.recruiterapp.recruiter_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruiterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruiterAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaletechs/recruiterapp/recruiter_app/RecruiterAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jaletechs.recruiterapp.recruiter_app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecruiterAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
